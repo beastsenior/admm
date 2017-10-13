9df9ed6b092d44e471b5dac19f9981b82a2fab50
beastsenior <beastsenior@gmail.com>
2017-10-13 00:47:03 -0400

    20171013_004703

--- renew_mask.py
+++ renew_mask.py
@@ -1,27 +1,26 @@
 import topology as to
-import net_interface as ni
 
 #creat topology
 tmp_to = to.rand_topology()
-for i in ni.IPLIST:
-	for j in ni.IPLIST:
+for i in to.IPLIST:
+	for j in to.IPLIST:
 		print(i, '-->', j, tmp_to[(i,j)])
 print('\n\n')
 to.save_topology(tmp_to)
 adjacency_matrix = to.load_topology()
-for i in ni.IPLIST:
-	for j in ni.IPLIST:
+for i in to.IPLIST:
+	for j in to.IPLIST:
 		print(i, '-->', j, adjacency_matrix[(i,j)])
 print('\n\n')
 
 topology_mask = to.get_full_mask(adjacency_matrix)
-for i in ni.IPLIST:
-	for j in ni.IPLIST:
+for i in to.IPLIST:
+	for j in to.IPLIST:
 		print(i, '-->', j, to.topology_mask[(i,j)])
 print('\n\n')
 to.save_mask(topology_mask)
 topology_mask = to.load_mask()
-for i in ni.IPLIST:
-	for j in ni.IPLIST:
+for i in to.IPLIST:
+	for j in to.IPLIST:
 		print(i, '-->', j, topology_mask[(i,j)])
 print('\n\n')
